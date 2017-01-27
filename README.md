18c1f53de25d87851c2b0f23e1cf260d102d95c8
Frida Sjoholm <frida@fridaesign.se>
2017-01-27 12:20:22 -0800

    Adds README

---
+++ README.md
@@ -0,0 +1,9 @@
+####Yodafying
+##Description
+This chrome extensions is made to transfer all big headers (h1, h2, h3) to something the amazing YODA would say. You can install the extension from the or download the repo and upload it in developer mode.
+[Alt text](relative/path/to/useYodify.gif?raw=true "Title")
+
+##Use this Repo
+Uses the Yoda Speak Generator APIhttp://www.yodaspeak.co.uk/.
+To learn more about the API and get your own key see here
+https://market.mashape.com/ismaelc/yoda-speak#!documentation.
--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+
 var headers = Array.from(document.querySelectorAll("h1, h2, h3"));
 headers.forEach(function(title) {
   var oldtitle = title.textContent;
   var oReq = new XMLHttpRequest();
   oReq.addEventListener("load", reqListener);
   oReq.open("GET", "https://yoda.p.mashape.com/yoda?sentence="+oldtitle);
-  oReq.setRequestHeader("X-Mashape-Key", /*YOUR API KEY*/);
+
+  oReq.setRequestHeader("X-Mashape-Key",/*YOUR API KEY*/);
   oReq.setRequestHeader("Content-Type", "text/plain;charset=UTF-8");
   oReq.send();
 
